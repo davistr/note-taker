2a161c9beea02fd370340635e749e3011ff203ce
davistr <davistr1216@gmail.com>
2022-02-23 17:57:21 -0500

    updated storage.js

--- db/storage.js
+++ db/storage.js
@@ -4,44 +4,48 @@ const util = require('util');
 
 
 // Use util.promisify to return reponses in a promise object
-const readNotes = util.promisify(fs.readFile);
-const writeNotes = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// create storage class for member methods to handle notes
+// create storage class for member methods to handle notes 
+const storage = {
+    // Return notes written to json file
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    },
+    // Write note to json file
+    write(notes) {
+        return writeFileAsync('db/db.json', JSON.stringify(notes));
+    },
+    // Return saved notes
+    getNotes() {
+        return this.read().then((notes) => {
 
-// Return notes written to json file
-const read = () => {
-    return readNotes('db/db.json', 'utf8');
-};
-// Write note to json file
-const write = note => {
-    return writeNotes('db/db.json', JSON.stringify(note));
-};
-// Return saved notes
-const getNotes = () => {
-    return this.read().then((notes) => {
+            let savedNotes;
+            try {
+                savedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                savedNotes = [];
+            }
+            return savedNotes;
+        });
+    },
+    // Only add new note if both note title and text fields have content
+    addNote(note) {
 
-        let savedNotes;
-        try {
-            savedNotes = [].concat(JSON.parse(notes));
-        } catch (err) {
-            savedNotes = [];
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error("Both 'title' and 'text' field cannot be empty!");
         }
-        return savedNotes;
-    });
-};
-// Only add new note if both note title and text fields have content
-const addNote = note => {
 
-    const { title, text } = note;
-    if (!title || text) {
-        throw new Error("Both 'title' and 'text' field cannot be empty!");
+        const newNote = { title, text };
+
+        return this.getNotes()
+            .then((notes) => {
+                const newArray = [...notes, newNote]
+                this.write(newArray)
+            });
     }
+}
 
-    const newNote = { title, text };
-
-    return this.getNotes()
-        .then((notes) => [...notes, newNote]);
-};
-
-module.exports = { read, write, getNotes, addNote };
+module.exports = storage;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,23 @@
 const router = require('express').Router();
-const { read, write, getNotes, addNote } = require('../db/storage');
+const storage = require('../db/storage');
 
 
 router.get('/notes', (req, res) => {
-    getNotes()
-        .then((notes) => {
-            return res.json(notes);
-        })
+    storage.getNotes().then((notes) => {
+        return res.json(notes);
+    })
         .catch((err) => res.status(500).json(err));
 });
 
-
 router.post('/notes', (req, res) => {
-    addNote(req.body)
-        .then((note) => res.json(note))
+    console.log(req.body);
+    storage.addNote(req.body).then((note) => res.json(note))
         .catch((err) => res.status(500).json(err));
 });
 
 router.delete('/notes/:id', (req, res) => {
-    removeNote(req.params.id)
+    storage
+        .removeNote(req.params.id)
         .then(() => res.json({ ok: true }))
         .catch((err) => res.status(500).json(err));
 });
